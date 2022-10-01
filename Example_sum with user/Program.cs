e929952c5a05defd637eba581b0d309613fde2fa
Elena <evagon4arova@gmail.com>
2022-10-01 18:56:52 +0500

    Пользователь вводит 2 целых числа и получает частное введенных чисел

---
+++ Example_sum with user/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит 2 целых числа и получает частное введенных чисел
+
+int numberA = 12;
+int numberB = 5;
+int result = numberA / numberB;
+Console.WriteLine(result);
+
+// Пользователь вводит 2 целых числа и получает частное введенных чисел
+
+// double numberA = 12;
+// double numberB = 5;
+// double result = numberA / numberB;
+// Console.WriteLine(result);
