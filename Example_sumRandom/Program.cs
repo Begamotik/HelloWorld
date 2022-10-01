4640ed7c962ecbee0d06c139af41140565c98490
Elena <evagon4arova@gmail.com>
2022-10-01 19:30:58 +0500

    Расчет суммы 2 цулых случайных чисел

---
+++ Example_sumRandom/Program.cs
@@ -0,0 +1,7 @@
+﻿// Сумма 2х случайных целых чисел через Рандом
+int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
