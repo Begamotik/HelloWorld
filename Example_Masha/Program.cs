487f4d906eb578f5b762c29d8bf3886fc140f8aa
Elena <evagon4arova@gmail.com>
2022-10-01 19:13:09 +0500

    Приветствие любимчика

---
+++ Example_Masha/Program.cs
@@ -0,0 +1,13 @@
+﻿// Как приветствовать любимчика-пользователя (Маша)
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine()!;
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это Маша!!!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
